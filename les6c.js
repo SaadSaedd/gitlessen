69ddc0a8264518a923b891468f0d00a840d2191f
saad <37354@ma-web.nl>
2023-11-03 10:36:15 +0100

    eerste

--- les6c.js
+++ les6c.js
@@ -1,18 +1,15 @@
-let stuff = ['c', 5645, 9393.77, "hello", true, false, "Good morning", 88, -90, 777.777, 90, 665.33, "F"];
-let strings = [];
-let numbers = [];
-let booleans = [];
+let f = ['c', 5645, 9393.77, "hello", true, false, "Good morning", 88, -90, 777.777, 90, 665.33, "F"];
+let string = [];
+let getal = [];
+
 
 for (let i = 0; i < stuff.length; i++) {
   if (typeof stuff[i] === "string") {
-    strings.push(stuff[i]);
+    string.push(stuff[i]);
   } else if (typeof stuff[i] === "number") {
     numbers.push(stuff[i]);
-  } else if (typeof stuff[i] === "boolean") {
-    booleans.push(stuff[i]);
-  }
+  
 }
-
-console.log("Strings array:", strings);
-console.log("Numbers array:", numbers);
-console.log("Booleans array:", booleans);
+}
+console.log("Strings array:", string);
+console.log("Numbers array:", getal);
