0bc1003f58191d29744924c577ef446cc360946d
saad <37354@ma-web.nl>
2023-11-03 12:22:53 +0100

    eerste

--- les6.js
+++ les6.js
@@ -1,32 +1,24 @@
 function sponge(stringa){
-    //maak resultaat variabel lege string = ""
-    //let gemoktestring = sponge("Ik heb een beker water");
     let resultaat =""
+    
+    return stringa.toLowerCase();
+    return stringb.toUppercase();
 
-
-    //for loop over stringa (dus voor elke letter)
-    for (let index = 0; index < stringa.length; index++) {
-    const letter = stringa[index];
-
-    let randomgetal = Math.random();
-
-    if(randomgetal <0.5){
-       resultaat+= letter .toUpperCase()
-    }else
-    resultaat+= letter .toLowerCase() 
 }
+let gemoktestring = sponge("Ik heb een beker water");
+console.log(">>"+gemoktestring);
 
-return resultaat
-    }
-
-
-        //random getal 0-1 en als if > 0.5 uppercase
-        //voeg de letter stringa[i] toe aan resultaat, soms met uppercase
-
-
-
-
-
-//IK heB Een BeKeR WATer
-console.log(sponge("hello world"));
-//deze les heb ik met Abdul,Damian en meester gemaakt!!!
+  function ShakeFunctie(woordDatGeschudtMoetWorden){
+    
+        const characters = woordDatGeschudtMoetWorden.split('');
+        
+          for (let i = characters.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            
+            stringa.toLowerCase();
+            stringb.toUppercase();
+          }
+          const geschudWoord = characters.join('');
+          	  	
+        }
+      
--- les6c.js
+++ les6c.js
@@ -1,15 +1,20 @@
 let f = ['c', 5645, 9393.77, "hello", true, false, "Good morning", 88, -90, 777.777, 90, 665.33, "F"];
 let string = [];
 let getal = [];
+let booleans = [];
 
 
-for (let i = 0; i < stuff.length; i++) {
-  if (typeof stuff[i] === "string") {
-    string.push(stuff[i]);
-  } else if (typeof stuff[i] === "number") {
-    numbers.push(stuff[i]);
-  
+for (let i = 0; i < f.length; i++) {
+  if (typeof f[i] === "string") {
+    string.push(f[i]);
+  } else if (typeof f[i] === "number") {
+    getal.push(f[i]);
+} else if (typeof f[i] === "booleans"){
+  booleans.push(stuff[i]);
 }
 }
+
 console.log("Strings array:", string);
 console.log("Numbers array:", getal);
+console.log("booleans", booleans);
+
